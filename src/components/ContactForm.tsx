fb5b8608e8554fc825f6672d306431ce9c24a9e1
Andrea Bettarini <bettarini@monema.it>
2023-06-29 09:15:26 +0200

    Fix validazione yup

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -73,8 +73,14 @@ export const ContactForm = () => {
                 <Formik
                     initialValues={ContactFormInitialValues}
                     validationSchema={Yup.object({
-                        name: Yup.string()
+                        first_name: Yup.string()
                             .max(15, "Must be 15 characters or less")
+                            .required("Required"),
+                        last_name: Yup.string()
+                            .max(20, "Must be 20 characters or less")
+                            .required("Required"),
+                        subject: Yup.string()
+                            .max(50, "Must be 50 characters or less")
                             .required("Required"),
                         email: Yup.string()
                             .email("Invalid email address")
